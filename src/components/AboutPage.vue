1d995c6b73dbb9839d2cea57b5ef9dcf79549e9c
RichtersHotline <KeenanTimmieWeb@gmail.com>
2024-07-02 08:11:10 +0200

    added json data

--- src/components/AboutPage.vue
+++ src/components/AboutPage.vue
@@ -1,125 +1,162 @@
 <template>
-  <div class="container-fluid">
-    <h1 class="text-center">About Me</h1> 
-    <h2 class="text-center">A brief introduction</h2> 
-    <div class="abouttxt mx-auto">
-   <p>Hello to whomever is reading this. As you probably guessed my name is Keenan Timme. I've been interested in computers ever since I can remember I must've been age 4 when I first started actively using computers. Ever since I always knew that my job had to do with computers in someway or another.</p>
-   <div class="AboutImg">
     <div class="container-fluid">
-        <div class="row text-center mx-auto">
-          <div class="col">
-            <img src="https://richtershotline.github.io/Pimages/Images/PortBalc3.jpg" class="img-fluid mt-3 mb-4" loading="lazy" alt="LandingPagePhoto">
+      <h1 class="text-center">About Me</h1> 
+      <h2 class="text-center">A brief introduction</h2> 
+      <div class="abouttxt mx-auto" v-if="aboutMe?.length">
+     <p>{{aboutMe[0]?.AboutMeText}}</p>
+     <div class="AboutImg">
+      <div class="container-fluid">
+          <div class="row text-center mx-auto" >
+            
+            <div class="col">
+              
+              <img src="https://richtershotline.github.io/Pimages/Images/PortBalc3.jpg" class="img-fluid mt-3 mb-4" loading="lazy" alt="LandingPagePhoto">
+            </div>
           </div>
-        </div>
-
-</div>
-
+         
   </div>
   
-   <h2 class="text-center">My Story</h2> 
-   <p>My story is somewhat common I would think. I am from Colorado Park, Mitchell's Plain. Not a nice area but hey beggars can't be choosers as they say. I started coding when i was around 12 my reasoning being was that I wanted to see how games worked. Looked it up and saw something called programming which looked like a foreign language to me. (Still does) And then kinda coded on and off. Self teaching myself with the Unity Game Engine, doing html and css on the side. Learning C# never got around to 3D modeling so all my playermodels were cylinders.</p>
-   <div class="AboutImg">
-    <div class="container-fluid">
-        <div class="row text-center mx-auto">
-          <div class="col">
-            <img src="https://richtershotline.github.io/Pimages/Images/Mitchalls_Plain_sign_rock.jpg" class="img-fluid mt-3 mb-4" loading="lazy" alt="LandingPagePhoto">
-          </div>
-        </div>
-
-</div>
-
-  </div>
-</div>
-<h2 class="text-center">My Aspirations</h2> 
-<div class="abouttxt mx-auto">
-    <p>Ever since I was in High School I felt like such a let down. I didn't get that good of grades I mean I didn't get zeroes and ones, I was very average but I always wanted to be more than average. I just wanted to show the world I can do it that I will do it. I've found my passion which is programming and I am willing to go as far as I need to to achieve my aspirations. I want to be the outlier. Where i'm from you're not supposed to "make it" you just settle for a retail job or a cleaner or whatever and that's it that's what you get if you're from Mitchell's Plain. I want to break that. I want to be a leader I want people from my area to go "He can make it so can I".</p>
-    <div class="AboutImg">
-        <div class="container-fluid">
-            <div class="row text-center mx-auto">
-              <div class="col">
-                <img src="https://richtershotline.github.io/Pimages/Images/Find-True-Freedom-1536x960.png" class="img-fluid mt-3 mb-4" loading="lazy" alt="LandingPagePhoto">
-              </div>
+    </div>
+    
+     <h2 class="text-center" v-if="aboutMe?.length">My Story</h2> 
+     <p>{{aboutMe[1]?.AboutMeText}}</p>
+     <div class="AboutImg">
+      <div class="container-fluid">
+          <div class="row text-center mx-auto">
+            <div class="col">
+              <img src="https://richtershotline.github.io/Pimages/Images/Mitchalls_Plain_sign_rock.jpg" class="img-fluid mt-3 mb-4" loading="lazy" alt="LandingPagePhoto">
             </div>
-    
-    </div>
-    
-      </div>
-
-</div>
-<h2 class="text-center">My Hobbies</h2> 
-<div class="container-fluid">
-    <div class="row text-center Hobbies g-0">
-      <div class="col">
-        <h2>Gaming</h2>
-        <h4>Favourite Game: Hotline Miami 2</h4>
-        <img src="https://richtershotline.github.io/Pimages/Images/HotlineMiami2_Cover.webp" class="img-fluid mt-3 mb-4" loading="lazy" alt="HobPhotos">
-      </div>
-      <div class="col">
-        <h2>Reading</h2>
-        <h4>Favourite Book: Game Of Thrones</h4>
-        <img src="https://richtershotline.github.io/Pimages/Images/GameThrones.jpg" class="img-fluid mt-3 mb-4" loading="lazy" alt="HobPhotos">
-      </div>
-      <div class="col">
-        <h2>Studying History</h2>
-        <h4>Favourite Topic: World War 2</h4>
-        <img src="https://richtershotline.github.io/Pimages/Images/Berlin.jpg" class="img-fluid mt-3 mb-4" loading="lazy" alt="HobPhotos">
-      </div>
-    </div>
-    
+          </div>
+  
   </div>
+  
+    </div>
+  </div>
+  <LoadingSpinner v-else>
+                          
+                        </LoadingSpinner>
+  <h2 class="text-center">My Aspirations</h2> 
+  <div class="abouttxt mx-auto" v-if="aboutMe?.length">
+      <p>{{aboutMe[2]?.AboutMeText}}</p>
+      <div class="AboutImg">
+          <div class="container-fluid">
+              <div class="row text-center mx-auto">
+                <div class="col">
+                  <img src="https://richtershotline.github.io/Pimages/Images/Find-True-Freedom-1536x960.png" class="img-fluid mt-3 mb-4" loading="lazy" alt="LandingPagePhoto">
+                </div>
+              </div>
+      
+      </div>
+      
+        </div>
+  
+  </div>
+  <LoadingSpinner v-else>
+                          
+                        </LoadingSpinner>
+                       
+  <h2 class="text-center">My Hobbies</h2> 
   <div class="container-fluid">
-    <h2 class="text-center">Miscellaneous Info</h2>
-    <div class="row mx-auto text-center Misc mt-4">
-      <div class="col">
-        <h2 class="text-center">Spoken Languages</h2> 
-        <h4>English - Fluent</h4>  
-        <h4>Spanish - Learning</h4> 
-        <h4>Afrikaans - Advanced</h4>
+      <div class="row text-center Hobbies g-0">
+        <div class="col" v-if="aboutMe?.length">
+          <h2>Gaming</h2>
+          <h4>{{aboutMe[3]?.HobbyFav}}</h4>
+          <img :src="aboutMe[3]?.HobbyImg" class="img-fluid mt-3 mb-4" loading="lazy" alt="HobPhotos">>
+        </div>
+        <LoadingSpinner v-else>
+                          
+                        </LoadingSpinner>
+        <div class="col" v-if="aboutMe?.length">
+          <h2>Reading</h2>
+          <h4>{{aboutMe[4]?.HobbyFav}}</h4>
+          <img :src="aboutMe[4]?.HobbyImg" class="img-fluid mt-3 mb-4" loading="lazy" alt="HobPhotos">>
+        </div>
+        <LoadingSpinner v-else>
+                          
+                        </LoadingSpinner>
+        <div class="col" v-if="aboutMe?.length">
+          <h2>Studying History</h2>
+          <h4>{{aboutMe[5]?.HobbyFav}}</h4>
+          <img :src="aboutMe[5]?.HobbyImg" class="img-fluid mt-3 mb-4" loading="lazy" alt="HobPhotos">>
+        </div>
+        <LoadingSpinner v-else>
+                          
+                        </LoadingSpinner>
       </div>
-      <div class="col">
-        <h2 class="text-center">Date Of Birth</h2> 
-        <h4>9 March 2002</h4>  
-      </div>
-      <div class="col">
-        <h2 class="text-center">Favourite Quote</h2> 
-   <h4>"The guy who gets knocked down isn't the loser it's the guy that can't get back up, he's the one who loses" -Daisuku Kuze</h4>
-      </div>
+      
     </div>
-    <div class="row mx-auto text-center Misc mt-4">
+    <div class="container-fluid">
+      <h2 class="text-center">Miscellaneous Info</h2>
+      <div class="row mx-auto text-center Misc mt-4">
+        <div class="col" v-if="aboutMe?.length">
+          <h2 class="text-center">Spoken Languages</h2> 
+          <h4>{{aboutMe[6]?.SpokenLang}}</h4>  
+          <h4>{{aboutMe[7]?.SpokenLang}}</h4> 
+          <h4>{{aboutMe[8]?.SpokenLang}}</h4>
+        </div>
+        <LoadingSpinner v-else>
+                          
+                        </LoadingSpinner>
         <div class="col">
-          <h2 class="text-center">Favourite End</h2> 
-          <h4>Frontend Development</h4> 
+          <h2 class="text-center">Date Of Birth</h2> 
+          <h4>9 March 2002</h4>  
         </div>
         <div class="col">
-          <h2 class="text-center">Favourite site</h2> 
-          <h4>Youtube</h4>  
+          <h2 class="text-center" v-if="aboutMe?.length">Favourite Quote</h2> 
+          <LoadingSpinner v-else>
+                          
+                        </LoadingSpinner>
+     <h4 v-if="aboutMe?.length">{{aboutMe[9]?.Quote}}</h4>
+     
         </div>
-        <div class="col">
-          <h2 class="text-center">Best Quality</h2> 
-     <h4>Loyal</h4>
-        </div>
+        
       </div>
+      
+      <div class="row mx-auto text-center Misc mt-4">
+          <div class="col">
+            <h2 class="text-center" v-if="aboutMe?.length">Favourite End</h2> 
+            <LoadingSpinner v-else>
+                          
+                        </LoadingSpinner>
+            <h4  v-if="aboutMe?.length">{{aboutMe[10]?.Dev}}</h4> 
+            
+          </div>
+          
+          <div class="col">
+            <h2 class="text-center" v-if="aboutMe?.length">Favourite site</h2> 
+            <LoadingSpinner v-else>
+                          
+                        </LoadingSpinner>
+            <h4 v-if="aboutMe?.length">{{aboutMe[11]?.Site}}</h4>  
+          </div>
+          <div class="col" v-if="aboutMe?.length">
+            <h2 class="text-center">Best Quality</h2> 
+       <h4 v-if="aboutMe?.length">{{aboutMe[12]?.Quality}}</h4>
+          </div>
+          <LoadingSpinner v-else>
+                          
+                        </LoadingSpinner>
+        </div>
+    </div>
+  
   </div>
-
-</div>
-</template>
-
-<script setup>
-// import {computed, onMounted} from "vue"
-// import {useStore} from "vuex"
-// // import LoadingSpinner from "./LoadingSpinner.vue"
-// // const store = useStore()
-// // const aboutMe = computed(() => store.state.aboutMe)
-
-
-// onMounted(() => {
-   
-//     store.dispatch("getAbout")
-//     // repeat()
-
-// })
-</script>
-
-<style>
-
-</style>
+  </template>
+  
+  <script setup>
+  import {computed, onMounted} from "vue"
+  import {useStore} from "vuex"
+  import LoadingSpinner from "./LoadingSpinner.vue"
+  const store = useStore()
+  const aboutMe = computed(() => store.state.aboutMe)
+  
+  
+  onMounted(() => {
+     
+      store.dispatch("getAbout")
+  
+  })
+  </script>
+  
+  <style>
+  
+  </style>
